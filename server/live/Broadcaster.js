1a61c04058a12166228839058de9ddf496710552
Watson Medeiros <watsonmedeiros@MacBook-Pro-de-Watson.local>
2026-07-01 10:30:44 -0300

    Debug broadcaster

--- server/live/Broadcaster.js
+++ server/live/Broadcaster.js
@@ -8,7 +8,12 @@ module.exports = class Broadcaster {
 
         const message = JSON.stringify(data);
 
+        console.log("📡 Broadcast:", message);
+        console.log("👥 Clientes conectados:", this.server.clients.size);
+
         this.server.clients.forEach(client => {
+
+            console.log("➡️ Enviando para cliente:", client.readyState);
 
             if(client.readyState === 1){
                 client.send(message);
